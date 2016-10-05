3743c368d9f429880a75a2357ba9ff6960ffb29d
Trevor Douglas <tdoug19@gmail.com>
2016-10-05 15:37:56 -0600

    We are committing our project

---
+++ src/com/trevordouglas/MyTestApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.trevordouglas;
+
+/**
+ * @author sseStudent_2
+ *
+ */
+public class MyTestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
